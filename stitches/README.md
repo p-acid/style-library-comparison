79638f8aa85e01ab80874d1f9fce35737f45d0e5
p-acid <qkrtkstjd01@gmail.com>
2022-12-09 16:18:40 +0900

    Add Config file

--- stitches/README.md
+++ stitches/README.md
@@ -6,3 +6,9 @@ npx create-next-app --example with-stitches
 ```
 
 > If you want to add the settings of stitches to cna project, check [this link](https://stitches.dev/blog/using-nextjs-with-stitches)
+
+## Packages
+
+```sh
+yarn add @stitches/react
+```
---
+++ stitches/stitches.config.ts
@@ -0,0 +1,56 @@
+import { createStitches } from "@stitches/react";
+
+export const { styled, getCssText } = createStitches({
+  /**
+   * 테마 데이터
+   */
+  theme: {
+    fonts: {
+      system: "system-ui",
+    },
+    colors: {
+      hiContrast: "hsl(206,10%,5%)",
+      loContrast: "white",
+    },
+    fontSizes: {
+      1: "13px",
+      2: "15px",
+      3: "17px",
+    },
+  },
+  /**
+   * 반응형 (미디어 쿼리)
+   */
+  media: {
+    bp1: "(min-width: 640px)",
+    bp2: "(min-width: 768px)",
+    bp3: "(min-width: 1024px)",
+    toBp2: "(width <= 768px)",
+  },
+  /**
+   * 커스텀 스타일 유틸
+   */
+  utils: {
+    size: (value: number) => ({
+      width: value,
+      height: value,
+    }),
+    linearGradient: (value: string) => ({
+      backgroundImage: `linear-gradient(${value})`,
+    }),
+    bg: (value: string | number) => ({
+      backgroundColor: value,
+    }),
+  },
+  /**
+   * 전역 충돌을 피하기 위한 클래스명 접두사
+   */
+  prefix: "radix",
+  /**
+   * 테마 토큰에 대한 CSS 요소 매핑
+   */
+  themeMap: {
+    width: "space",
+    height: "space",
+  },
+});
